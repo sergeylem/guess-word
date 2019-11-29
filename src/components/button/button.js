6f1f7c772cedbe0b15d1ba8adac2d02700bfce4d
Sergey Lem <sergey.lem@gmail.com>
2019-11-29 23:01:38 +0600

    changed button style

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './button.css';
 
 const Button = (props) => {
   return (
-    <div className='button'
-      onClick = {props.click} >
-        <span>{props.letter}</span>
+    <div 
+        className = 'button'
+        onClick = {props.click} >
+        <div>{props.letter}</div>
     </div>
   );
 }
